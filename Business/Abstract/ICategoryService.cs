84eefdc0904dc67368e147a032521bf093aa834c
Büşra Yıldırım <busra_yldrrm@hotmail.com.tr>
2021-02-13 23:55:06 +0300

    Add project files.

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GelAll();
+        Category GetById(int categoryId);
+
+
+    }
+}
---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        //iş kodları
+        ICategoryDal _categoryDal;
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+
+            _categoryDal = categoryDal;
+        }
+        public List<Category> GelAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categoryDal.Get(c =>c.CategoryId == categoryId);
+        }
+
+        
+    }
+}
